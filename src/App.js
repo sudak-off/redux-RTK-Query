7fcdc13adfc8af02b48e9752f4f8ae61a68411fd
Артем <artem@MacBook-Air-Artem.local>
2022-02-21 21:38:35 +0200

    Создали через greateApi некий редьюсер с дополнительными супер возможностями.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useGetGoodsQuery } from "./redux";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { data = [], isLoading } = useGetGoodsQuery();
+	if (isLoading) return <h1>Loading...</h1>;
+	return (
+		<div className="App">
+			<ul>
+				{data.map((item) => (
+					<li key={item.id}>{item.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
