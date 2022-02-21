a9ea49c1afc8b011d2549646c7c86ccebb5172a9
Артем <artem@MacBook-Air-Artem.local>
2022-02-21 22:07:31 +0200

    пример кэширования

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { useGetGoodsQuery } from "./redux";
 import "./App.css";
 
 function App() {
-	const { data = [], isLoading } = useGetGoodsQuery();
+	const [count, setCount] = useState("");
+	const { data = [], isLoading } = useGetGoodsQuery(count);
 	if (isLoading) return <h1>Loading...</h1>;
 	return (
-		<div className="App">
+		<div>
+			<div>
+				<select value={count} onChange={(e) => setCount(e.target.value)}>
+					<option value="''">all</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+				</select>
+			</div>
 			<ul>
 				{data.map((item) => (
 					<li key={item.id}>{item.name}</li>
